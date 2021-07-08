b69071cb175637832094de6ca87f4e9ea1c7fc22
Ott021 <ot.pacheco@duocuc.cl>
2021-07-08 12:01:36 -0400

    Autentificar y Autorizar Listo!

--- MiProyectoGroundZero/rest_ground/urls.py
+++ MiProyectoGroundZero/rest_ground/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from rest_ground.views import detalle_obra,detalle_usuario, lista_obras, lista_usuarios
+from .viewslogin import login
 
 
 urlpatterns = [
     path('usuarios',lista_usuarios,name="lista_usuarios"),
     path('usuarios/<id>',detalle_usuario,name="detalle_usuario"),
     path('obras',lista_obras,name="lista_obras"),
-    path('obras/<id>',detalle_obra,name="detalle_obra")
+    path('obras/<id>',detalle_obra,name="detalle_obra"),
+    path('login',login, name='login')
 ]
--- MiProyectoGroundZero/rest_ground/views.py
+++ MiProyectoGroundZero/rest_ground/views.py
@@ -6,14 +6,15 @@ from rest_ground.serializers import UsuarioSerializer
 from django.shortcuts import render
 from core.models import Usuario,Producto
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.parsers import JSONParser
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 
 @csrf_exempt
 @api_view(['GET','POST'])
-
+@permission_classes((IsAuthenticated,))
 def lista_usuarios(request):
     if request.method == 'GET':
         usuarios = Usuario.objects.all()
@@ -29,6 +30,7 @@ def lista_usuarios(request):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  
 
 @api_view(['GET','PUT','DELETE'])
+@permission_classes((IsAuthenticated,))
 def detalle_usuario(request, id):
     try:
         usuario = Usuario.objects.get(rut=id)              
@@ -55,7 +57,7 @@ def detalle_usuario(request, id):
 
 @csrf_exempt
 @api_view(['GET','POST'])
-
+@permission_classes((IsAuthenticated,))
 def lista_obras(request):
     if request.method == 'GET':
         obras = Producto.objects.all()
@@ -72,6 +74,7 @@ def lista_obras(request):
 
 
 @api_view(['GET','PUT','DELETE'])
+@permission_classes((IsAuthenticated,))
 def detalle_obra(request, id):
     try:
         obra = Producto.objects.get(rut=id)              
