7b263078bdff8f0c504527adb99329657b79648c
Ott021 <ot.pacheco@duocuc.cl>
2021-07-08 02:53:18 -0400

    full api, obras en proceso

--- MiProyectoGroundZero/rest_ground/serializers.py
+++ MiProyectoGroundZero/rest_ground/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import fields, serializers
-from core.models import Usuario
+from core.models import Producto, Usuario
 
 class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Usuario
+        fields = '__all__'
+
+class ObraSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Producto
         fields = '__all__'
--- MiProyectoGroundZero/rest_ground/urls.py
+++ MiProyectoGroundZero/rest_ground/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from rest_ground.views import lista_usuarios
+from rest_ground.views import detalle_usuario, lista_obras, lista_usuarios
 
 
 urlpatterns = [
-    path('usuarios',lista_usuarios,name="lista_usuarios")
+    path('usuarios',lista_usuarios,name="lista_usuarios"),
+    path('usuarios/<id>',detalle_usuario,name="detalle_usuario"),
+    path('obras',lista_obras,name="lista_obras")
 ]
